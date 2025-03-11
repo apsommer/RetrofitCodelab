a834c1a02583ba189ede631e8bba797194fa6ef4
Drew Sommer <drew@sommerengineering.com>
2025-03-11 13:32:49 +0100

    version: Add retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,8 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // retrofit 
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 }
